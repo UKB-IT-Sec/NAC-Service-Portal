9a89ae3071e3e2a7e2d709a135f73080912799c5
Aksiznarf-Uar <franziska.rau@mailbox.org>
2024-05-17 13:19:27 +0200

    Devices kann jetzt ein User zugewiesen werden

--- src/nac/models.py
+++ src/nac/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Device(models.Model):
     name = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name[:50]
