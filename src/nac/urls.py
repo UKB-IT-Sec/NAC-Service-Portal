a1d06e8d3c02945701bd36ed4f85df6290253830
Aksiznarf-Uar <franziska.rau@mailbox.org>
2024-05-20 22:21:56 +0200

    Working on editing functionality for devices

--- src/nac/urls.py
+++ src/nac/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import HomePageView, DevicePageView
+from .views import (
+    HomePageView,
+    DevicePageView,
+    DeviceDetailView,
+    DeviceUpdateView,
+    DeviceDeleteView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("devices/", DevicePageView.as_view(), name="devices"),
+    path("devices/<int:pk>/", DeviceDetailView.as_view(), name="device_detail"),
+    path("devices/<int:pk>/edit/", DeviceUpdateView.as_view(), name="device_edit"),
+    path("devices/<int:pk>/delete", DeviceDeleteView.as_view(), name="device_delete"),
 ]
--- src/nac/views.py
+++ src/nac/views.py
@@ -1,10 +1,29 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Device
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+
 class DevicePageView(ListView):
     model = Device
     template_name = "devices.html"
+
+
+class DeviceDetailView(DetailView):
+    model = Device
+    template_name = "device_detail.html"
+
+
+class DeviceUpdateView(UpdateView):
+    model = Device
+    fields = ("name", "area", "securitygroup",)
+    template_name = "device_edit.html"
+
+class DeviceDeleteView(DeleteView):
+    model = Device
+    template_name = "device_delete.html"
+    success_url = reverse_lazy("devices")
