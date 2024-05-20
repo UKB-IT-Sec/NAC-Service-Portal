e55a673c8a9499a554cab718e275d8329ffb43d8
Aksiznarf-Uar <franziska.rau@mailbox.org>
2024-05-20 15:21:46 +0200

    Added Area and Device Models

--- src/nac/models.py
+++ src/nac/models.py
@@ -2,9 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class SecurityGroup(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name[:50]
+
+
+class Area(models.Model):
+    name = models.TextField()
+    user = models.ManyToManyField(User)
+    security_group = models.ManyToManyField(SecurityGroup)
+
+    def __str__(self):
+        return self.name[:50]
+
+
 class Device(models.Model):
     name = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
+    security_group = models.ForeignKey(SecurityGroup, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name[:50]
