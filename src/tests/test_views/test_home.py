770dcdd1b8af98feec22b0c5eb227f11f5e52ee3
Peter Weidenbach <weidenba@gmail.com>
2024-06-04 16:17:37 +0200

    tests improved

--- src/tests/test_views/test_home.py
+++ src/tests/test_views/test_home.py
@@ -3,7 +3,15 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_view(client):
+def test_home(client):
     url = reverse('home')
     response = client.get(url)
+    assert b'You are not logged in.' in response.content
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_login_page(client):
+    response = client.get('/accounts/login/')
+    assert b'login' in response.content
     assert response.status_code == 200
