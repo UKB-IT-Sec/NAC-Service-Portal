bab1763e871ad0ed52e20e9dfad566a3b522f9dd
Dan1elRoos <roos_daniel@gmx.de>
2024-07-12 10:46:43 +0200

    Added Mac_Addresses for DeviceList, Added Icon-Symbols

--- src/nac/models.py
+++ src/nac/models.py
@@ -26,7 +26,8 @@ class CustomUser(AbstractUser):
 class Device(models.Model):
     name = models.CharField(max_length=100)
     area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
-    security_group = models.ForeignKey(SecurityGroup, on_delete=models.SET_NULL, null=True)
+    security_group = models.ForeignKey(SecurityGroup,
+                                       on_delete=models.SET_NULL, null=True)
     synchronized = models.BooleanField(null=True, default=False)
 
     appl_NAC_FQDN = models.CharField(null=True, max_length=100)
@@ -38,10 +39,13 @@ class Device(models.Model):
     appl_NAC_AllowAccessAIR = models.BooleanField(null=True)
     appl_NAC_AllowAccessVPN = models.BooleanField(null=True)
     appl_NAC_AllowAccessCEL = models.BooleanField(null=True)
-    appl_NAC_DeviceRoleProd = models.CharField(null=True, blank=True, max_length=100)
-    appl_NAC_DeviceRoleInst = models.CharField(null=True, blank=True, max_length=100)
+    appl_NAC_DeviceRoleProd = models.CharField(null=True,
+                                               blank=True, max_length=100)
+    appl_NAC_DeviceRoleInst = models.CharField(null=True,
+                                               blank=True, max_length=100)
     appl_NAC_macAddressCAB = models.TextField(null=True, blank=True)
-    appl_NAC_macAddressAIR = models.CharField(null=True, max_length=100, blank=True)
+    appl_NAC_macAddressAIR = models.CharField(null=True,
+                                              max_length=100, blank=True)
     appl_NAC_Certificate = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -49,3 +53,14 @@ class Device(models.Model):
 
     def get_absolute_url(self):
         return reverse("device_detail", kwargs={"pk": self.pk})
+
+    def split_MAC(self):
+        def format_mac(mac):
+            return ":".join(mac[i:i+2] for i in range(0, 12, 2)) \
+                if mac else None
+        return {
+            "appl_NAC_macAddressAIR":
+                [format_mac(self.appl_NAC_macAddressAIR)],
+            "appl_NAC_macAddressCAB": [format_mac(mac) for mac in
+                                       self.appl_NAC_macAddressCAB.split(",")]
+        }
