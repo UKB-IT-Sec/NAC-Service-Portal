9a4c3109ea890318de94cc6034e2b227c4357000
Dan1elRoos <roos_daniel@gmx.de>
2024-07-17 13:44:26 +0200

    Added formatted MAC-Addresses for DeviceList and Device Overview, Added Icon-Symbols

--- src/nac/models.py
+++ src/nac/models.py
@@ -54,13 +54,16 @@ class Device(models.Model):
     def get_absolute_url(self):
         return reverse("device_detail", kwargs={"pk": self.pk})
 
-    def split_MAC(self):
-        def format_mac(mac):
-            return ":".join(mac[i:i+2] for i in range(0, 12, 2)) \
-                if mac else None
-        return {
-            "appl_NAC_macAddressAIR":
-                [format_mac(self.appl_NAC_macAddressAIR)],
-            "appl_NAC_macAddressCAB": [format_mac(mac) for mac in
-                                       self.appl_NAC_macAddressCAB.split(",")]
-        }
+    def format_mac(self, mac):
+        return ":".join(mac[i:i+2] for i in range(0, 12, 2))
+
+    def get_appl_NAC_macAddressAIR(self):
+        if not self.appl_NAC_macAddressAIR:
+            return None
+        return [self.format_mac(self.appl_NAC_macAddressAIR)]
+
+    def get_appl_NAC_macAddressCAB(self):
+        if not self.appl_NAC_macAddressCAB:
+            return None
+        return [self.format_mac(mac) for mac in
+                self.appl_NAC_macAddressCAB.split(",")]
