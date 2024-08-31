0762558a15bfc6ea994683a4a95f9410937df1aa
Dan1elRoos <roos_daniel@gmx.de>
2024-08-31 08:14:51 +0200

    fix flake8

--- src/nac/management/commands/export_ldapObjects.py
+++ src/nac/management/commands/export_ldapObjects.py
@@ -3,7 +3,7 @@ import logging
 from helper.filesystem import get_config_directory
 from helper.config import get_config_from_file
 from helper.logging import setup_console_logger
-from helper.ldap import connect_to_ldap_server, map_device_data
+from helper.ldap import connect_to_ldap_server
 from ldap3 import SUBTREE
 from csv import DictWriter
 from os import stat
@@ -12,12 +12,17 @@ DEFAULT_CONFIG = get_config_directory() / 'export.cnf'
 DEFAULT_OBJECT = 'inetOrgPerson'
 CSV_SAVE_FILE = 'ldapObjects.csv'
 
+
 class Command(BaseCommand):
     help = "Export objects from LDAP server"
 
     def add_arguments(self, parser):
-        parser.add_argument('-c', '--config_file', default=DEFAULT_CONFIG, help='use a specific config file [src/export.cnf]')
-        parser.add_argument('-o', '--objectclass', default=DEFAULT_OBJECT, help='specify which object to export')
+        parser.add_argument('-c', '--config_file',
+                            default=DEFAULT_CONFIG,
+                            help='use a specific config file [src/export.cnf]')
+        parser.add_argument('-o', '--objectclass',
+                            default=DEFAULT_OBJECT,
+                            help='specify which object to export')
 
     def handle(self, *args, **options):
         setup_console_logger(options['verbosity'])
@@ -40,37 +45,48 @@ class Command(BaseCommand):
     def _handle_objects(self):
         logging.info('Initiating CSV export to file: %s', CSV_SAVE_FILE)
         for progress_counter, entry in enumerate(self.ldap_connection.entries):
-            self.write_object(entry.entry_attributes_as_dict, progress_counter+1)
+            self.write_object(
+                entry.entry_attributes_as_dict, progress_counter+1)
 
     def _get_objects(self):
-        logging.debug('Searching for %s-objects',self.objectClass)
-        search_result =  self.ldap_connection.search('dc=ukbonn,dc=de','(objectclass={})'.format(self.objectClass),SUBTREE,attributes=['*'])
+        logging.debug('Searching for %s-objects', self.objectClass)
+        search_result = self.ldap_connection.search(
+            'dc=ukbonn,dc=de', '(objectclass={})'.format(self.objectClass),
+            SUBTREE, attributes=['*'])
         self.entry_count = len(self.ldap_connection.response)
         if search_result:
-            logging.info('Found: %s-objects - Count: %i',self.objectClass, self.entry_count)
+            logging.info('Found: %s-objects - Count: %i',
+                         self.objectClass, self.entry_count)
         else:
-            logging.error("Not Found: %s-objects",self.objectClass)
-    
-    def _convert_dictlists_to_str(self,dict):
-        return {key: ','.join(value) if isinstance(value,list) else str(value) for key,value in dict.items()}
-            
+            logging.error("Not Found: %s-objects",
+                          self.objectClass)
+
+    def _convert_dictlists_to_str(self, dict):
+        return {key: ','.join(value) if isinstance(value, list)
+                else str(value) for key, value in dict.items()}
+
     def _clear_file(self):
         try:
-            logging.info('Truncating %s',CSV_SAVE_FILE)
-            with open(CSV_SAVE_FILE,'w'):
+            logging.info('Truncating %s', CSV_SAVE_FILE)
+            with open(CSV_SAVE_FILE, 'w'):
                 pass
         except Exception as e:
             logging.error('Failed to truncate %s -> %s', CSV_SAVE_FILE, str(e))
 
-    def write_object(self,object_as_dict,progress_counter):
+    def write_object(self, object_as_dict, progress_counter):
         try:
             column_header = object_as_dict.keys()
-            with open(CSV_SAVE_FILE,'a',newline="") as csvfile:
-                writer = DictWriter(csvfile, fieldnames=column_header, delimiter=';')
+            with open(CSV_SAVE_FILE, 'a', newline="") as csvfile:
+                writer = DictWriter(
+                    csvfile, fieldnames=column_header, delimiter=';')
                 if stat(CSV_SAVE_FILE).st_size == 0:
                     writer.writeheader()
-                writer.writerows([self._convert_dictlists_to_str(object_as_dict)])
-                logging.debug('Successfully exported object %i/%i to file: %s ', progress_counter, self.entry_count, CSV_SAVE_FILE,)
+                writer.writerows(
+                    [self._convert_dictlists_to_str(object_as_dict)])
+                logging.debug(
+                    'Successfully exported object %i/%i to file: %s ',
+                    progress_counter, self.entry_count, CSV_SAVE_FILE,)
         except Exception as e:
-            logging.error('Failed to export object to file: %s -> %s',CSV_SAVE_FILE, str(e))
-        
+            logging.error(
+                'Failed to export object to file: %s -> %s',
+                CSV_SAVE_FILE, str(e))
--- src/tests/test_commands/test_export_ldapObjects.py
+++ src/tests/test_commands/test_export_ldapObjects.py
@@ -1,8 +1,8 @@
 import pytest
-from nac.management.commands.export_ldapObjects import Command, CSV_SAVE_FILE, DEFAULT_OBJECT, DEFAULT_CONFIG
+from nac.management.commands.export_ldapObjects import Command, \
+    CSV_SAVE_FILE, DEFAULT_OBJECT, DEFAULT_CONFIG
 from unittest.mock import patch, mock_open, MagicMock
 from configparser import ConfigParser
-from argparse import ArgumentParser
 from ldap3 import SUBTREE
 
 
@@ -14,10 +14,12 @@ def command():
 def test_add_arguments(command):
     mock_parser = MagicMock()
     command.add_arguments(mock_parser)
-    mock_parser.add_argument.assert_any_call('-c', '--config_file', default=DEFAULT_CONFIG, help='use a specific config file [src/export.cnf]')
-    mock_parser.add_argument.assert_any_call('-o', '--objectclass', default=DEFAULT_OBJECT, help='specify which object to export')
-
-
+    mock_parser.add_argument.assert_any_call(
+        '-c', '--config_file', default=DEFAULT_CONFIG,
+        help='use a specific config file [src/export.cnf]')
+    mock_parser.add_argument.assert_any_call(
+        '-o', '--objectclass', default=DEFAULT_OBJECT,
+        help='specify which object to export')
 
 
 @patch('nac.management.commands.export_ldapObjects.setup_console_logger')
@@ -26,14 +28,14 @@ def test_add_arguments(command):
 @patch('nac.management.commands.export_ldapObjects.Command._clear_file')
 @patch('nac.management.commands.export_ldapObjects.Command._get_objects')
 @patch('nac.management.commands.export_ldapObjects.Command._handle_objects')
-def test_handle(mock_handle_objects, mock_get_objects, mock_clear_file, 
-                mock_connect_to_ldap, mock_get_config, mock_setup_logger, command):
+def test_handle(mock_handle_objects, mock_get_objects, mock_clear_file,
+                mock_connect_to_ldap, mock_get_config, mock_setup_logger,
+                command):
     options = {
         'verbosity': 0,
         'config_file': 'test.cnf',
         'objectclass': 'testObject'
     }
-    
     mock_config = ConfigParser()
     mock_config['ldap-server'] = {
         'address': 'test_address',
@@ -44,9 +46,7 @@ def test_handle(mock_handle_objects, mock_get_objects, mock_clear_file,
     }
     print(mock_config)
     mock_get_config.return_value = mock_config
-    
     command.handle(**options)
-    
     mock_setup_logger.assert_called_once_with(0)
     mock_get_config.assert_called_once_with('test.cnf')
     mock_clear_file.assert_called_once()
@@ -55,7 +55,6 @@ def test_handle(mock_handle_objects, mock_get_objects, mock_clear_file,
     )
     mock_get_objects.assert_called_once()
     mock_handle_objects.assert_called_once()
-    
     assert command.config == mock_config
     assert command.objectClass == 'testObject'
 
@@ -68,9 +67,7 @@ def test_handle_objects(mock_write_object, command):
     mock_entry2.entry_attributes_as_dict = {'attr2': 'value2'}
     command.ldap_connection = MagicMock()
     command.ldap_connection.entries = [mock_entry1, mock_entry2]
-    
     command._handle_objects()
-
     assert mock_write_object.call_count == 2
     mock_write_object.assert_any_call({'attr1': 'value1'}, 1)
     mock_write_object.assert_any_call({'attr2': 'value2'}, 2)
@@ -82,11 +79,12 @@ def test_get_objects_found(mock_logging, command):
     command.ldap_connection.search.return_value = True
     command.ldap_connection.response = ['obj1', 'obj2']
     command.objectClass = 'testObject'
-    
     command._get_objects()
-    
-    command.ldap_connection.search.assert_called_once_with('dc=ukbonn,dc=de', '(objectclass=testObject)', SUBTREE, attributes=['*'])
-    mock_logging.info.assert_called_once_with('Found: %s-objects - Count: %i', 'testObject', 2)
+    command.ldap_connection.search.assert_called_once_with(
+        'dc=ukbonn,dc=de', '(objectclass=testObject)',
+        SUBTREE, attributes=['*'])
+    mock_logging.info.assert_called_once_with(
+        'Found: %s-objects - Count: %i', 'testObject', 2)
 
 
 @patch('nac.management.commands.export_ldapObjects.logging')
@@ -95,17 +93,19 @@ def test_get_objects_not_found(mock_logging, command):
     command.ldap_connection.search.return_value = False
     command.ldap_connection.response = []
     command.objectClass = 'testObject'
-    
     command._get_objects()
-    
-    command.ldap_connection.search.assert_called_once_with('dc=ukbonn,dc=de', '(objectclass=testObject)', SUBTREE, attributes=['*'])
-    mock_logging.error.assert_called_once_with('Not Found: %s-objects','testObject')
+    command.ldap_connection.search.assert_called_once_with(
+        'dc=ukbonn,dc=de', '(objectclass=testObject)',
+        SUBTREE, attributes=['*'])
+    mock_logging.error.assert_called_once_with(
+        'Not Found: %s-objects', 'testObject')
 
 
 def test_convert_dictlists_to_str(command):
     test_dict = {'key1': ['value1', 'value2'], 'key2': 'value3'}
     result = command._convert_dictlists_to_str(test_dict)
     assert result == {'key1': 'value1,value2', 'key2': 'value3'}
+
 
 @patch('builtins.open', new_callable=mock_open)
 @patch('nac.management.commands.export_ldapObjects.logging')
@@ -119,47 +119,54 @@ def test_clear_file(mock_logging, mock_file, command):
 @patch('nac.management.commands.export_ldapObjects.logging')
 def test_clear_file_exception(mock_logging, mock_file, command):
     command._clear_file()
-    mock_logging.error.assert_called_once_with('Failed to truncate %s -> %s', CSV_SAVE_FILE, 'Failed')
+    mock_logging.error.assert_called_once_with(
+        'Failed to truncate %s -> %s', CSV_SAVE_FILE, 'Failed')
 
 
 @patch('builtins.open', new_callable=mock_open)
 @patch('nac.management.commands.export_ldapObjects.DictWriter')
 @patch('nac.management.commands.export_ldapObjects.stat')
 @patch('nac.management.commands.export_ldapObjects.logging')
-def test_write_object_empty_file(mock_logging, mock_stat, mock_writer, mock_file, command):
+def test_write_object_empty_file(mock_logging, mock_stat,
+                                 mock_writer, mock_file, command):
     object_dict = {'attr1': 'value1', 'attr2': ['value2', 'value3']}
     mock_stat.return_value.st_size = 0
     mock_writer_instance = MagicMock()
     mock_writer.return_value = mock_writer_instance
     command.entry_count = 10
-    
     command.write_object(object_dict, 5)
-    
     mock_file.assert_called_once_with(CSV_SAVE_FILE, 'a', newline="")
-    mock_writer.assert_called_once_with(mock_file(), fieldnames=object_dict.keys(), delimiter=';')
+    mock_writer.assert_called_once_with(
+        mock_file(), fieldnames=object_dict.keys(), delimiter=';')
     mock_writer_instance.writeheader.assert_called_once()
-    mock_writer_instance.writerows.assert_called_once_with([{'attr1': 'value1', 'attr2': 'value2,value3'}])
-    mock_logging.debug.assert_called_once_with('Successfully exported object %i/%i to file: %s ', 5, 10, CSV_SAVE_FILE)
+    mock_writer_instance.writerows.assert_called_once_with(
+        [{'attr1': 'value1', 'attr2': 'value2,value3'}])
+    mock_logging.debug.assert_called_once_with(
+        'Successfully exported object %i/%i to file: %s ',
+        5, 10, CSV_SAVE_FILE)
 
 
 @patch('builtins.open', new_callable=mock_open)
 @patch('nac.management.commands.export_ldapObjects.DictWriter')
 @patch('nac.management.commands.export_ldapObjects.stat')
 @patch('nac.management.commands.export_ldapObjects.logging')
-def test_write_object_non_empty_file(mock_logging, mock_stat, mock_writer, mock_file, command):
+def test_write_object_non_empty_file(mock_logging, mock_stat,
+                                     mock_writer, mock_file, command):
     object_dict = {'attr1': 'value1', 'attr2': ['value2', 'value3']}
     mock_stat.return_value.st_size = 100
     mock_writer_instance = MagicMock()
     mock_writer.return_value = mock_writer_instance
     command.entry_count = 10
-    
     command.write_object(object_dict, 5)
-    
     mock_file.assert_called_once_with(CSV_SAVE_FILE, 'a', newline="")
-    mock_writer.assert_called_once_with(mock_file(), fieldnames=object_dict.keys(), delimiter=';')
+    mock_writer.assert_called_once_with(
+        mock_file(), fieldnames=object_dict.keys(), delimiter=';')
     mock_writer_instance.writeheader.assert_not_called()
-    mock_writer_instance.writerows.assert_called_once_with([{'attr1': 'value1', 'attr2': 'value2,value3'}])
-    mock_logging.debug.assert_called_once_with('Successfully exported object %i/%i to file: %s ', 5, 10, CSV_SAVE_FILE)
+    mock_writer_instance.writerows.assert_called_once_with(
+        [{'attr1': 'value1', 'attr2': 'value2,value3'}])
+    mock_logging.debug.assert_called_once_with(
+        'Successfully exported object %i/%i to file: %s ',
+        5, 10, CSV_SAVE_FILE)
 
 
 @patch('builtins.open', side_effect=Exception('Failed'))
@@ -167,9 +174,8 @@ def test_write_object_non_empty_file(mock_logging, mock_stat, mock_writer, mock_
 def test_write_object_exception(mock_logging, mock_file, command):
     object_dict = {'attr1': 'value1', 'attr2': ['value2', 'value3']}
     mock_writer_instance = MagicMock()
-
     command.write_object(object_dict, 5)
-
     mock_writer_instance.writeheader.assert_not_called()
     mock_writer_instance.writerows.assert_not_called()
-    mock_logging.error.assert_called_once_with('Failed to export object to file: %s -> %s', CSV_SAVE_FILE, 'Failed')
+    mock_logging.error.assert_called_once_with(
+        'Failed to export object to file: %s -> %s', CSV_SAVE_FILE, 'Failed')
