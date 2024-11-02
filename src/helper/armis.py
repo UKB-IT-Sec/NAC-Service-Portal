5f68b5f81c94971e3bbc106d4a2b5cd2406a92ac
Dan1elRoos <roos_daniel@gmx.de>
2024-11-02 14:44:49 +0100

    add decorator for acloud-setup

--- src/helper/armis.py
+++ src/helper/armis.py
@@ -3,39 +3,45 @@ from helper.filesystem import get_config_directory
 from armis import ArmisCloud
 from .database import MacList
 import re
+from functools import wraps
 
-global_acloud = None
 armis_config = get_config_from_file(get_config_directory() / 'armis.cfg')
 
 
-def get_or_create_armis_cloud():  # connects to armis cloud
+def armiscloud(func):
     global global_acloud
-    if global_acloud is None:
-        global_acloud = ArmisCloud(
-            api_secret_key=armis_config['armis-server']['api_secret_key'],
-            tenant_hostname=armis_config['armis-server']['tenant_hostname']
-        )
-    return global_acloud
+    global_acloud = None
+
+    @wraps(func)
+    def get_or_create_armis_cloud(*args, **kwargs):
+        global global_acloud
+        if global_acloud is None:
+            global_acloud = ArmisCloud(
+                api_secret_key=armis_config['armis-server']['api_secret_key'],
+                tenant_hostname=armis_config['armis-server']['tenant_hostname']
+            )
+        return func(global_acloud, *args, **kwargs)
+    return get_or_create_armis_cloud
 
 
-def _filter_sort_sites(sites):  # removes all sites that dont match the regex-Pattern
+def _filter_sort_sites(sites):
     pattern = rf"{armis_config['armis-server']['sites_pattern']}"
     filtered_sites = {key: value for key, value in sites.items() if re.match(pattern, value['name'])}
     return dict(sorted(filtered_sites.items(), key=lambda x: x[1]['name']))
 
 
-def get_armis_sites():
-    acloud = get_or_create_armis_cloud()
+@armiscloud
+def get_armis_sites(acloud):
     return _filter_sort_sites(acloud.get_sites())
 
 
-def _remove_existing_devices(deviceList):  # removes armis devices that already exist in the Database based on the hostname
+def _remove_existing_devices(deviceList):
     _mac_list = MacList()
     return [device for device in deviceList if not _mac_list.check_existing_mac(device)[0]]
 
 # flake8: noqa: E231
-def get_devices(site):  # gets Devices based on a specific site, can be filtered via vlan blacklist
-    acloud = get_or_create_armis_cloud()
+@armiscloud
+def get_devices(acloud, site):
     vlan_bl = ""
     vlan_blacklist = armis_config['armis-server'].get('vlan_blacklist', '')
     vlan_bl = f"!networkInterface:(vlans:{vlan_blacklist})" if vlan_blacklist else ""
--- src/tests/test_helper/test_armis.py
+++ src/tests/test_helper/test_armis.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from helper.armis import (
-    get_or_create_armis_cloud,
+    armiscloud,
     _filter_sort_sites,
     get_armis_sites,
     _remove_existing_devices,
@@ -28,19 +28,23 @@ def mock_armis_cloud():
 
 @patch('helper.armis.armis_config', new_callable=MagicMock)
 @patch('helper.armis.ArmisCloud')
-def test_get_or_create_armis_cloud(mock_armis_cloud, mock_armis_config, mock_config):
+def test_armiscloud_decorator(mock_armis_cloud, mock_armis_config, mock_config):
     mock_armis_config.__getitem__.return_value = mock_config['armis-server']
     mock_armis_cloud_instance = mock_armis_cloud.return_value
 
-    result = get_or_create_armis_cloud()
+    @armiscloud
+    def test_func(acloud):
+        return acloud
+
+    result = test_func()
     assert result == mock_armis_cloud_instance
     mock_armis_cloud.assert_called_once_with(
         api_secret_key='test_key',
         tenant_hostname='test_host'
     )
 
-    result2 = get_or_create_armis_cloud()
-    assert result2 == result  # check if its the same Cloud
+    result2 = test_func()
+    assert result2 == result  # check if it's the same Cloud
     assert mock_armis_cloud.call_count == 1
 
 
@@ -57,45 +61,43 @@ def test_filter_sort_sites(mock_config):
     assert [site['name'] for site in result.values()] == ['Site1', 'Site2', 'Site3']
 
 
-@patch('helper.armis.get_or_create_armis_cloud')
 @patch('helper.armis._filter_sort_sites')
-def test_get_armis_sites(mock_filter_sort_sites, mock_get_or_create_armis_cloud):
-    mock_cloud = MagicMock()
-    mock_get_or_create_armis_cloud.return_value = mock_cloud
+def test_get_armis_sites(mock_filter_sort_sites):
     mock_sites = {'1': {'name': 'Site1'}}
-    mock_cloud.get_sites.return_value = mock_sites
+    mock_armis_cloud = MagicMock()
+    mock_armis_cloud.get_sites.return_value = mock_sites
     mock_filter_sort_sites.return_value = mock_sites
+    #  manually call original function without decorator to prevent argument error
+    result = get_armis_sites.__wrapped__(mock_armis_cloud)
 
-    result = get_armis_sites()
     assert result == mock_sites
-    mock_cloud.get_sites.assert_called_once()
+    mock_armis_cloud.get_sites.assert_called_once()
     mock_filter_sort_sites.assert_called_once_with(mock_sites)
 
 
 @patch('helper.armis.MacList')
 def test_remove_existing_devices(mock_mac_list):
-    mock_mac_list = mock_mac_list.return_value
-    mock_mac_list.check_existing_mac.side_effect = [(True, None), (False, None), (True, None)]
+    mock_mac_list_instance = mock_mac_list.return_value
+    mock_mac_list_instance.check_existing_mac.side_effect = [(True, None), (False, None), (True, None)]
 
     devices = [{'name': 'Device1'}, {'name': 'Device2'}, {'name': 'Device3'}]
     result = _remove_existing_devices(devices)
     assert result == [{'name': 'Device2'}]
 
 
-@patch('helper.armis.get_or_create_armis_cloud')
 @patch('helper.armis._remove_existing_devices')
-def test_get_devices(mock_remove_existing_devices, mock_get_or_create_armis_cloud, mock_config):
-    mock_cloud = MagicMock()
-    mock_get_or_create_armis_cloud.return_value = mock_cloud
+def test_get_devices(mock_remove_existing_devices, mock_config):
     mock_devices = [{'id': '1', 'name': 'Device1'}]
-    mock_cloud.get_devices.return_value = mock_devices
+    mock_armis_cloud = MagicMock()
+    mock_armis_cloud.get_devices.return_value = mock_devices
     mock_remove_existing_devices.return_value = mock_devices
 
+    #  manually call original function without decorator to prevent argument error
     with patch('helper.armis.armis_config', mock_config):
-        result = get_devices({'name': 'TestSite'})
+        result = get_devices.__wrapped__(mock_armis_cloud, {'name': 'TestSite'})
 
     assert result == mock_devices
-    mock_cloud.get_devices.assert_called_once_with(
+    mock_armis_cloud.get_devices.assert_called_once_with(
         asq='in:devices site:"TestSite" timeFrame:"7 Days" !networkInterface:(vlans:100,200)',
         fields_wanted=['id', 'ipAddress', 'macAddress', 'name', 'boundaries']
     )
