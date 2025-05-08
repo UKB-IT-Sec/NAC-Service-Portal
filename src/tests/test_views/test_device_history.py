579da98635ce78724122a7c6593a2d6eb3e7fac4
Ley Berster <ley.berster@web.de>
2025-05-08 15:35:53 +0200

    (tests wip) test_select_none and test_delete_none

--- src/tests/test_views/test_device_history.py
+++ src/tests/test_views/test_device_history.py
@@ -87,3 +87,65 @@ def test_delete_version(hostname):
     assert response["Location"] == url
 
     assert not device_with_history.history.filter(history_id=history_entry.history_id).exists()
+
+
+@pytest.mark.django_db
+def test_select_none():
+    device_with_history = Device.objects.get(id=1)
+    device_with_history.appl_NAC_Hostname = "dev1-v1"
+    device_with_history.save()
+
+    expected_data = model_to_dict(device_with_history)
+
+    test_user = CustomUser.objects.create(name="test")
+    test_user.authorization_group.set([AuthorizationGroup.objects.get(pk=1), AuthorizationGroup.objects.get(pk=2)])
+
+    rf = RequestFactory()
+    url = f"/devices/{device_with_history.pk}/edit/"
+    post_data = {
+        "select": "1",
+        "device_version": "",
+    }
+    request = rf.post(url, post_data)
+    request.user = test_user
+    request.path = url
+
+    view = DeviceUpdateView()
+    view.setup(request, pk=device_with_history.pk)
+    view.object = view.get_object()
+    response = view.post(request, pk=device_with_history.pk)
+
+    assert response.status_code == 200
+    form = response.context_data["form"]
+    assert form.initial["appl_NAC_Hostname"] == expected_data["appl_NAC_Hostname"]
+
+
+@pytest.mark.django_db
+def test_delete_none():
+    device_with_history = Device.objects.get(id=1)
+    device_with_history.appl_NAC_Hostname = "dev1-v1"
+    device_with_history.save()
+
+    expected_data = model_to_dict(device_with_history)
+
+    test_user = CustomUser.objects.create(name="test")
+    test_user.authorization_group.set([AuthorizationGroup.objects.get(pk=1), AuthorizationGroup.objects.get(pk=2)])
+
+    rf = RequestFactory()
+    url = f"/devices/{device_with_history.pk}/edit/"
+    post_data = {
+        "delete": "1",
+        "device_version": "",
+    }
+    request = rf.post(url, post_data)
+    request.user = test_user
+    request.path = url
+
+    view = DeviceUpdateView()
+    view.setup(request, pk=device_with_history.pk)
+    view.object = view.get_object()
+    response = view.post(request, pk=device_with_history.pk)
+
+    assert response.status_code == 200
+    form = response.context_data["form"]
+    assert form.initial["appl_NAC_Hostname"] == expected_data["appl_NAC_Hostname"]
