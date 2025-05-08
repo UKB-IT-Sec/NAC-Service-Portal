5acd0592cf76f4b4ba49633051f346b78cd8ccc6
Ley Berster <ley.berster@web.de>
2025-05-08 15:27:50 +0200

    (tests wip) test_select_version and test_delete_version

---
+++ src/tests/test_views/test_device_history.py
@@ -0,0 +1,89 @@
+import pytest
+from nac.subviews.device_management import DeviceUpdateView
+from nac.models import AuthorizationGroup, Device, CustomUser
+from django.test import RequestFactory
+from django.forms.models import model_to_dict
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("hostname",
+                         [("dev1-v2"),
+                          ("dev1-v3"),
+                          ("dev1-v4")])
+def test_select_version(hostname):
+    device_with_history = Device.objects.get(id=1)
+    device_with_history.appl_NAC_Hostname = "dev1-v1"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v2"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v3"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v4"
+    device_with_history.save()
+
+    history_entry = device_with_history.history.get(appl_NAC_Hostname=hostname)
+    expected_data = model_to_dict(history_entry)
+
+    test_user = CustomUser.objects.create(name="test")
+    test_user.authorization_group.set([AuthorizationGroup.objects.get(pk=1), AuthorizationGroup.objects.get(pk=2)])
+
+    rf = RequestFactory()
+    url = f"/devices/{device_with_history.pk}/edit/"
+    post_data = {
+        "select": "1",
+        "device_version": history_entry.history_id,
+    }
+    request = rf.post(url, post_data)
+    request.user = test_user
+    request.path = url
+
+    view = DeviceUpdateView()
+    view.setup(request, pk=device_with_history.pk)
+    view.object = view.get_object()
+    response = view.post(request, pk=device_with_history.pk)
+
+    assert response.status_code == 200
+    form = response.context_data["form"]
+    assert form.initial["appl_NAC_Hostname"] == expected_data["appl_NAC_Hostname"]
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("hostname",
+                         [("dev1-v2"),
+                          ("dev1-v3"),
+                          ("dev1-v4")])
+def test_delete_version(hostname):
+    device_with_history = Device.objects.get(id=1)
+    device_with_history.appl_NAC_Hostname = "dev1-v1"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v2"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v3"
+    device_with_history.save()
+    device_with_history.appl_NAC_Hostname = "dev1-v4"
+    device_with_history.save()
+
+    history_entry = device_with_history.history.all().get(appl_NAC_Hostname=hostname)
+
+    test_user = CustomUser.objects.create(name="test")
+    test_user.authorization_group.set([AuthorizationGroup.objects.get(pk=1), AuthorizationGroup.objects.get(pk=2)])
+
+    rf = RequestFactory()
+    url = f"/devices/{device_with_history.pk}/edit/"
+    post_data = {
+        "delete": "1",
+        "device_version": history_entry.history_id,
+    }
+    request = rf.post(url, post_data)
+    request.user = test_user
+    request.path = url
+
+    view = DeviceUpdateView()
+    view.setup(request, pk=device_with_history.pk)
+    view.object = view.get_object()
+    response = view.post(request, pk=device_with_history.pk)
+
+    assert response.status_code == 302
+    assert response["Location"] == url
+
+    assert not device_with_history.history.filter(history_id=history_entry.history_id).exists()
