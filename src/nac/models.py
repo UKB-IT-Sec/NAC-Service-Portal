ffc78a32881f6e6e2af1bffb6f117e36280a3125
Dan1elRoos <roos_daniel@gmx.de>
2025-05-17 10:11:06 +0200

    add new attribute, change delete function

--- src/nac/models.py
+++ src/nac/models.py
@@ -72,6 +72,7 @@ class Device(models.Model):
     appl_NAC_macAddressAIR = models.CharField(null=True, max_length=100,
                                               blank=True, unique=True, verbose_name="MAC Address WiFi")
     appl_NAC_Certificate = models.TextField(null=True, blank=True, verbose_name="Certificate")
+    deleted = models.BooleanField(null=True, default=False, verbose_name="Deleted")
     history = HistoricalRecords()
 
     @property
