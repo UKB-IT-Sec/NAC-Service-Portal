05f90bcb204b0861145045a9d01928cd80c25414
Dan1elRoos <roos_daniel@gmx.de>
2025-07-03 09:07:56 +0200

    fix pytest

--- src/tests/test_helper/test_ldap.py
+++ src/tests/test_helper/test_ldap.py
@@ -22,10 +22,10 @@ def test_map_device_data():
     test_device = Device()
     result = map_device_data(test_device)
     assert isinstance(result, dict)
-    assert len(result) == 9
+    assert len(result) == 13
     test_device.appl_NAC_macAddressCAB = 'aabbccddeeff'
     result = map_device_data(test_device)
-    assert len(result) == 10
+    assert len(result) == 14
     assert result['appl-NAC-macAddressCAB'] == 'aabbccddeeff'
 
 
@@ -33,15 +33,15 @@ def test_device_exists():
     connection = _setup_test_server()
 
     assert not device_exists('NoneExistingDevice', connection, SEARCH_BASE)
-    assert device_exists('testDeviceCable', connection, SEARCH_BASE)
+    assert device_exists('testDeviceCable.ukbonn.de', connection, SEARCH_BASE)
 
     connection.unbind()
 
 
 def test_delete_device():
     connection = _setup_test_server()
-    assert device_exists('testDeviceCable', connection, SEARCH_BASE)
+    assert device_exists('testDeviceCable.ukbonn.de', connection, SEARCH_BASE)
 
-    delete_device('testDeviceCable', connection, SEARCH_BASE)
+    delete_device('testDeviceCable.ukbonn.de', connection, SEARCH_BASE)
 
-    assert not device_exists('testDeviceCable', connection, SEARCH_BASE)
+    assert not device_exists('testDeviceCable.ukbonn.de', connection, SEARCH_BASE)
