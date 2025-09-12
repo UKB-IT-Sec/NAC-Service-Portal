080f54dfc448649ee57ae797838880347ad6f796
Aksiznarf-Uar <franziska.rau@mailbox.org>
2025-09-12 11:49:42 +0200

    fixed flake8

--- src/nac/mixins.py
+++ src/nac/mixins.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
+
 class CustomPermissionsRequiredMixin(PermissionRequiredMixin):
     def get_permission_denied_message(self):
         return "You do not have sufficient permissions to access this page."
